3a25971f1425cb4357533275b394173bd8b81ce3
JCMendezzzzz <mjcmendez@tip.edu.ph>
2025-05-21 10:19:55 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,9 +1,11 @@
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  email VARCHAR(100) UNIQUE,
-  password VARCHAR(255)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE grades (
