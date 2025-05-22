72081dce724e990c114fbddc98c493c3e732ccd9
Jerome-321 <jeeromee7@gmail.com>
2025-05-23 02:56:39 +0800

    Create profile.js

---
+++ routes/profile.js
@@ -0,0 +1,7 @@
+// src/routes/profile.js
+import { Router } from "express";
+const router = Router();
+
+// For this example, your profile routes are handled inline in server.js,
+// so just export an empty router here to satisfy the import.
+export default router;
