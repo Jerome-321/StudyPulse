f30811d75a3a8a48bcc507d4a9db636bfda745df
Jerome-321 <jeeromee7@gmail.com>
2025-05-23 08:50:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 require('dotenv').config();
